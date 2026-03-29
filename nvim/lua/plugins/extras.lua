2f04d1950ce0b0f43ddbc752488114da7331d19b
paranjay <kparanjay245@gmail.com>
2026-03-29 16:57:30 +0530

    fix(nvim): remove conflicting LazyVim extras
    Removed telescope and mini-starter extras that conflict with
    newer LazyVim's built-in snacks_picker and snacks_dashboard.
    These are enabled by default in recent LazyVim versions.

--- nvim/lua/plugins/extras.lua
+++ nvim/lua/plugins/extras.lua
@@ -1,20 +1,8 @@
--- LazyVim extras to enable
 return {
-  -- Lang extras
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.python" },
-  
-  -- Editor extras
-  { import = "lazyvim.plugins.extras.editor.telescope" },
   { import = "lazyvim.plugins.extras.editor.mini-files" },
-  
-  -- UI extras
-  { import = "lazyvim.plugins.extras.ui.mini-starter" },
-  
-  -- Formatting
   { import = "lazyvim.plugins.extras.formatting.prettier" },
-  
-  -- Linting
   { import = "lazyvim.plugins.extras.linting.eslint" },
 }
