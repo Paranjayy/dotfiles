c18ea9849de827bd27c67198ab51282524877dfc
Paranjay <kparanjay245@gmail.com>
2026-08-19 22:03:43 +0530

    fix(raycast): resolve correct homebrew path for omniwmctl

---
+++ raycast/extensions/omniwm/raycast-env.d.ts
@@ -0,0 +1,52 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `run-command` command */
+  export type RunCommand = ExtensionPreferences & {}
+  /** Preferences accessible in the `visualize-shortcuts` command */
+  export type VisualizeShortcuts = ExtensionPreferences & {}
+  /** Preferences accessible in the `search-windows` command */
+  export type SearchWindows = ExtensionPreferences & {}
+  /** Preferences accessible in the `switch-workspace` command */
+  export type SwitchWorkspace = ExtensionPreferences & {}
+  /** Preferences accessible in the `toggle-bar` command */
+  export type ToggleBar = ExtensionPreferences & {}
+  /** Preferences accessible in the `settings` command */
+  export type Settings = ExtensionPreferences & {}
+  /** Preferences accessible in the `backup-configs` command */
+  export type BackupConfigs = ExtensionPreferences & {}
+  /** Preferences accessible in the `restore-configs` command */
+  export type RestoreConfigs = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `run-command` command */
+  export type RunCommand = {}
+  /** Arguments passed to the `visualize-shortcuts` command */
+  export type VisualizeShortcuts = {}
+  /** Arguments passed to the `search-windows` command */
+  export type SearchWindows = {}
+  /** Arguments passed to the `switch-workspace` command */
+  export type SwitchWorkspace = {}
+  /** Arguments passed to the `toggle-bar` command */
+  export type ToggleBar = {}
+  /** Arguments passed to the `settings` command */
+  export type Settings = {}
+  /** Arguments passed to the `backup-configs` command */
+  export type BackupConfigs = {}
+  /** Arguments passed to the `restore-configs` command */
+  export type RestoreConfigs = {}
+}
+
